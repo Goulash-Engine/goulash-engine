8f38def8584cf9b1e52beb1b9d5c32ebf597a068
Charlie <xetra11@outlook.com>
2022-07-19 14:18:13 +0200

    feat(simulation): add simulation

---
+++ src/main/kotlin/com/barbarus/prosper/Simulation.kt
@@ -0,0 +1,10 @@
+package com.barbarus.prosper
+
+import com.barbarus.prosper.core.domain.Village
+
+class Simulation {
+    private val village = Village()
+
+    fun addClan() {
+    }
+}
---
+++ src/test/kotlin/com/barbarus/prosper/SimulationTest.kt
@@ -0,0 +1,14 @@
+package com.barbarus.prosper
+
+import assertk.assertThat
+import assertk.assertions.isTrue
+import org.junit.jupiter.api.Test
+
+class SimulationTest {
+    private val simulation = Simulation()
+
+    @Test
+    fun `should run simulation`() {
+        assertThat(true).isTrue()
+    }
+}
