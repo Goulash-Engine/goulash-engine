7859c7ee43e0bbef58c332c1b607b8fa5282a28b
Charlie <xetra11@outlook.com>
2022-07-19 11:22:33 +0200

    feat(processor): extend resource processor logic

--- src/main/kotlin/com/barbarus/prosper/processor/ResourceProcessor.kt
+++ src/main/kotlin/com/barbarus/prosper/processor/ResourceProcessor.kt
@@ -1,11 +1,17 @@
 package com.barbarus.prosper.processor
 
 import com.barbarus.prosper.core.domain.Profession
+import com.barbarus.prosper.core.domain.ProfessionType
 import com.barbarus.prosper.core.domain.Resource
 import com.barbarus.prosper.core.domain.ResourceType
 
 class ResourceProcessor {
     fun process(profession: Profession): Resource {
-        return Resource(ResourceType.FOOD, 1.0)
+        return when (profession.type) {
+            ProfessionType.GATHERER -> Resource(ResourceType.FOOD, 1.0)
+            ProfessionType.WOODWORKER -> Resource(ResourceType.WOODEN_MATERIAL, 1.0)
+            ProfessionType.TOOLMAKER -> TODO()
+            ProfessionType.HERBALIST -> TODO()
+        }
     }
 }
--- src/test/kotlin/com/barbarus/prosper/processor/ResourceProcessorTest.kt
+++ src/test/kotlin/com/barbarus/prosper/processor/ResourceProcessorTest.kt
@@ -11,6 +11,15 @@ internal class ResourceProcessorTest {
     private val resourceProcessor = ResourceProcessor()
 
     @Test
+    fun `should generate a wooden material resource when the profession type is woodworker`() {
+        val profession = Profession(ProfessionType.WOODWORKER)
+
+        val resource = resourceProcessor.process(profession)
+
+        assertThat(resource.type).isEqualTo(ResourceType.WOODEN_MATERIAL)
+    }
+
+    @Test
     fun `should generate a food resource when the profession type is gatherer`() {
         val profession = Profession(ProfessionType.GATHERER, 2.0)
 
