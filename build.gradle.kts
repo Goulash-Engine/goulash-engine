13c798bc5200f311d5629f5a6692b00be36e34fe
Charlie <xetra11@outlook.com>
2022-07-19 12:15:05 +0200

    feat(linter): add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
     id("java")
     id("groovy")
     application
