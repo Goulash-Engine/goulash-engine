5df9bac15fd9a68537d7d850f61d62d9dd365d89
Charlie <xetra11@outlook.com>
2022-07-26 08:43:51 +0200

    test(script): assure that exception if thrown if an unknown section name is used

--- src/test/kotlin/com/barbarus/prosper/script/parser/GlobalBlockerConditionScriptParserTest.kt
+++ src/test/kotlin/com/barbarus/prosper/script/parser/GlobalBlockerConditionScriptParserTest.kt
@@ -3,10 +3,25 @@ package com.barbarus.prosper.script.parser
 import assertk.assertThat
 import assertk.assertions.containsAll
 import com.barbarus.prosper.script.domain.GlobalBlockerCondition
+import com.barbarus.prosper.script.exception.UnknownSectionException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 internal class GlobalBlockerConditionScriptParserTest {
     private val globalBlockerConditionScriptParser = GlobalBlockerConditionScriptParser()
+
+    @Test
+    fun `should throw exception if section identifier is unknown`() {
+        val scriptData = """
+            [FooBar]
+            - foo
+            - bar
+        """.trimIndent()
+
+        assertThrows<UnknownSectionException> {
+            val actual: GlobalBlockerCondition = globalBlockerConditionScriptParser.parse(scriptData)
+        }
+    }
 
     @Test
     fun `should parse a globalblockercondition object from given script data`() {
