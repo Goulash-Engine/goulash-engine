c42fb0d5c66043f91263e9feba75838916754cf8
Charlie <xetra11@outlook.com>
2022-07-27 16:50:03 +0200

    feat(script): add logic to process simple script

--- src/main/kotlin/com/barbarus/prosper/script/grammar/LogicScriptFileGrammar.kt
+++ src/main/kotlin/com/barbarus/prosper/script/grammar/LogicScriptFileGrammar.kt
@@ -1,5 +1,11 @@
 package com.barbarus.prosper.script.grammar
 
+import com.barbarus.prosper.core.domain.Civilisation
+import com.barbarus.prosper.script.domain.LogicStatement
+import com.barbarus.prosper.script.domain.ScriptedLogic
+import com.github.h0tk3y.betterParse.combinators.map
+import com.github.h0tk3y.betterParse.combinators.or
+import com.github.h0tk3y.betterParse.combinators.separatedTerms
 import com.github.h0tk3y.betterParse.combinators.times
 import com.github.h0tk3y.betterParse.combinators.unaryMinus
 import com.github.h0tk3y.betterParse.combinators.use
@@ -7,20 +13,58 @@ import com.github.h0tk3y.betterParse.grammar.Grammar
 import com.github.h0tk3y.betterParse.lexer.literalToken
 import com.github.h0tk3y.betterParse.lexer.regexToken
 
-class LogicScriptFileGrammar : Grammar<String>() {
+class LogicScriptFileGrammar : Grammar<ScriptedLogic<Civilisation>>() {
     private val space by regexToken("\\s+", ignore = true)
     private val newLine by literalToken("\n", ignore = true)
 
     private val scriptKeyword by literalToken("logic")
+    private val identifier by regexToken("^[a-z]+")
     private val scriptName by regexToken("^[a-z_]+")
+    private val startLogicBlock by literalToken("{")
+    private val contextMutationOperator by literalToken("::")
+    private val operationOperator by literalToken(".")
+    private val digit by regexToken("^[\\d.\\d]+")
+    private val rightPar by literalToken(")")
+    private val leftPar by literalToken("(")
+    private val endLogicBlock by literalToken("}")
+    private val endOfStatement by literalToken(";", ignore = true)
 
-    private val scriptHeadParser by -scriptKeyword * scriptName
-    // private val statementParser by scriptHeadParser
+    /**
+     * logic <name>
+     */
+    private val scriptHeadParser by -scriptKeyword * (scriptName or identifier) use { ScriptHead(text) }
 
-    /** ScriptStatementGrammar()*/
+    /**
+     * .plus(1)
+     */
+    private val operationParser by -operationOperator * identifier * -leftPar * (digit or identifier) * -rightPar map { (name, argument) ->
+        ScriptStatementGrammar.Operation(name.text, argument.text)
+    }
 
-    override val rootParser by scriptHeadParser use { text } /*map { (scriptName, statements) ->
-        ScriptedLogic<Civilisation>("foo") { context ->
+    /**
+     * ::urge(eat)[.plus(1)]
+     */
+    private val mutationParser by -contextMutationOperator * identifier * -leftPar * identifier * -rightPar * operationParser map { (type, target, operation) ->
+        ScriptStatementGrammar.ContextMutation(type.text, target.text, operation)
+    }
+
+    /**
+     * { actors[::urge(eat)[.plus(1)]]]; }
+     */
+    private val contextCommandParser by identifier * mutationParser
+    private val statementParser by contextCommandParser map { (context, mutation) ->
+        LogicStatement(
+            context.text,
+            mutation.type,
+            mutation.target,
+            mutation.operation.name,
+            mutation.operation.argument
+        )
+    }
+
+    private val statementsParser by -startLogicBlock * separatedTerms(statementParser, endOfStatement) * -endLogicBlock
+    override val rootParser by (scriptHeadParser * statementsParser) map { (scriptHead, statements) ->
+        ScriptedLogic<Civilisation>(scriptHead.name) { context ->
             statements.forEach { statement ->
                 if (statement.context == "actors") {
                     if (statement.mutationType == "urge") {
@@ -38,5 +82,20 @@ class LogicScriptFileGrammar : Grammar<String>() {
                 }
             }
         }
-    }*/
+    }
+
+    internal class ScriptHead(
+        val name: String
+    )
+
+    internal data class ContextMutation(
+        val type: String,
+        val target: String,
+        val operation: Operation
+    )
+
+    internal data class Operation(
+        val name: String,
+        val argument: String
+    )
 }
--- src/test/kotlin/com/barbarus/prosper/script/grammar/LogicScriptFileGrammarTest.kt
+++ src/test/kotlin/com/barbarus/prosper/script/grammar/LogicScriptFileGrammarTest.kt
@@ -2,6 +2,8 @@ package com.barbarus.prosper.script.grammar
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import com.barbarus.prosper.core.domain.Civilisation
+import com.barbarus.prosper.script.domain.ScriptedLogic
 import com.github.h0tk3y.betterParse.grammar.parseToEnd
 import org.junit.jupiter.api.Test
 
@@ -10,19 +12,15 @@ internal class LogicScriptFileGrammarTest {
 
     @Test
     fun `should parse simple logic statement`() {
-        // val scriptData = """
-        //     logic my_foo {
-        //         actors::urge(eat).plus(1);
-        //         actors::urge(eat).plus(0.5);
-        //     }
-        // """.trimIndent()
         val scriptData = """
-            logic my_foo
+            logic myfoo {
+                actors::urge(eat).plus(1);
+                actors::urge(eat).plus(0.5);
+            }
         """.trimIndent()
 
-        val actual: String = logicScriptFileGrammar.parseToEnd(scriptData)
-        // val actual: ScriptedLogic<Civilisation> = logicScriptFileGrammar.parseToEnd(scriptData)
+        val actual: ScriptedLogic<Civilisation> = logicScriptFileGrammar.parseToEnd(scriptData)
 
-        assertThat(actual).isEqualTo("my_foo")
+        assertThat(actual.name).isEqualTo("myfoo")
     }
 }
