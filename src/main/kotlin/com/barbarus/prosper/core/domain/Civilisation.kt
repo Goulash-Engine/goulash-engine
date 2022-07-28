ea03b753c670c7bd193e0e9d7571c43ca5bed85f
Charlie <xetra11@outlook.com>
2022-07-28 10:15:04 +0200

    feat(logic): replace civilisation logic with scripted logic

--- src/main/kotlin/com/barbarus/prosper/core/domain/Civilisation.kt
+++ src/main/kotlin/com/barbarus/prosper/core/domain/Civilisation.kt
@@ -1,7 +1,6 @@
 package com.barbarus.prosper.core.domain
 
 import ScriptLoader
-import com.barbarus.prosper.civilisation.logic.CivilisationLogic
 
 /**
  * The core entity of the prosper engine. Within a village multiple [Clan]s try to survive and prosper through work,
@@ -10,11 +9,9 @@ import com.barbarus.prosper.civilisation.logic.CivilisationLogic
 class Civilisation(
     val actors: MutableList<Actor> = mutableListOf()
 ) {
-    private val civilisationLogic = CivilisationLogic()
 
     fun act() {
         ScriptLoader.getLogicScripts().forEach { it.process(this) }
-        civilisationLogic.process(this)
         actors.forEach { it.act() }
     }
 }
