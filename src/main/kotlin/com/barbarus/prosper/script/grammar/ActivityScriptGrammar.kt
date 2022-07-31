da476f6cd7494313940d1181ac359a8bc557a2b7
Charlie <xetra11@outlook.com>
2022-07-31 15:39:52 +0200

    feat(grammar): add logic to parse multiple trigger urges

--- src/main/kotlin/com/barbarus/prosper/script/grammar/ActivityScriptGrammar.kt
+++ src/main/kotlin/com/barbarus/prosper/script/grammar/ActivityScriptGrammar.kt
@@ -3,6 +3,7 @@ package com.barbarus.prosper.script.grammar
 import com.barbarus.prosper.script.logic.ActivityScriptContext
 import com.barbarus.prosper.script.logic.ScriptHead
 import com.github.h0tk3y.betterParse.combinators.map
+import com.github.h0tk3y.betterParse.combinators.separatedTerms
 import com.github.h0tk3y.betterParse.combinators.times
 import com.github.h0tk3y.betterParse.combinators.unaryMinus
 import com.github.h0tk3y.betterParse.combinators.use
@@ -17,15 +18,19 @@ internal class ActivityScriptGrammar : Grammar<ActivityScriptContext>() {
     private val triggerKeyword by literalToken("trigger")
     private val openBraces by literalToken("{")
     private val closeBraces by literalToken("}")
+    private val comma by literalToken(",")
     private val identifier by regexToken("^[a-z]+")
 
     private val activityNameParser by -identifier * identifier use { ScriptHead(text) }
-    private val triggerParser by -triggerKeyword * -openBraces * identifier * -closeBraces use { text }
+    private val triggerParser by -triggerKeyword * -openBraces * separatedTerms(
+        identifier,
+        comma
+    ) * -closeBraces map { terms -> terms.map { it.text } }
     private val activityBodyParser by -openBraces * triggerParser * -closeBraces
 
     // private val activityFunctions by identifier * -openBraces * identifier -openBraces * identifier
 
     override val rootParser by activityNameParser * activityBodyParser map { (scriptHead, triggers) ->
-        ActivityScriptContext(scriptHead, listOf(triggers))
+        ActivityScriptContext(scriptHead, triggers)
     }
 }
--- src/test/kotlin/com/barbarus/prosper/script/grammar/ActivityScriptGrammarTest.kt
+++ src/test/kotlin/com/barbarus/prosper/script/grammar/ActivityScriptGrammarTest.kt
@@ -1,7 +1,7 @@
 package com.barbarus.prosper.script.grammar
 
 import assertk.assertThat
-import assertk.assertions.contains
+import assertk.assertions.containsAll
 import assertk.assertions.isEqualTo
 import com.github.h0tk3y.betterParse.grammar.parseToEnd
 import org.junit.jupiter.api.Test
@@ -16,13 +16,13 @@ internal class ActivityScriptGrammarTest {
     fun `should parse simple activity script with name and trigger`() {
         val scriptData = """
             activity eat {
-                trigger { eat }
+                trigger { eat, enjoy }
             }
         """.trimIndent()
 
         val scriptContext = activityGrammar.parseToEnd(scriptData)
 
         assertThat(scriptContext.head.name).isEqualTo("eat")
-        assertThat(scriptContext.triggerUrges).contains("eat")
+        assertThat(scriptContext.triggerUrges).containsAll("eat", "enjoy")
     }
 }
