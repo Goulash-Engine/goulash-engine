06ed92ac0abc95c79a7bd07f5f43b47abea53c40
Charlie <xetra11@outlook.com>
2022-08-03 00:30:36 +0200

    refactor(grammar): replace old logic parser with statement grammar

--- src/main/kotlin/com/barbarus/prosper/script/grammar/ContainerScriptGrammar.kt
+++ src/main/kotlin/com/barbarus/prosper/script/grammar/ContainerScriptGrammar.kt
@@ -1,16 +1,13 @@
 package com.barbarus.prosper.script.grammar
 
-import com.barbarus.prosper.script.domain.ScriptStatement
 import com.barbarus.prosper.script.logic.ContainerScriptContext
 import com.barbarus.prosper.script.logic.ScriptHead
 import com.github.h0tk3y.betterParse.combinators.map
-import com.github.h0tk3y.betterParse.combinators.optional
-import com.github.h0tk3y.betterParse.combinators.or
-import com.github.h0tk3y.betterParse.combinators.separatedTerms
 import com.github.h0tk3y.betterParse.combinators.times
 import com.github.h0tk3y.betterParse.combinators.unaryMinus
 import com.github.h0tk3y.betterParse.combinators.use
 import com.github.h0tk3y.betterParse.grammar.Grammar
+import com.github.h0tk3y.betterParse.grammar.parseToEnd
 import com.github.h0tk3y.betterParse.lexer.literalToken
 import com.github.h0tk3y.betterParse.lexer.regexToken
 
@@ -18,57 +15,22 @@ internal class ContainerScriptGrammar : Grammar<ContainerScriptContext>() {
     private val space by regexToken("\\s+", ignore = true)
     private val newLine by literalToken("\n", ignore = true)
 
-    private val logicKeyword by literalToken("logic")
-    private val filter by regexToken("\\[(.*)\\]")
-    private val contextMutationOperator by literalToken("::")
-    private val identifier by regexToken("^[a-z]+")
-    private val scriptName by regexToken("^[a-z_]+")
+    private val logicKeyword by regexToken("^logic\\s*[a-z_]+\\s*")
+    private val logicBlock by regexToken("^[actor[\\[\\]a-z.<>=\\d\\s]*::[a-z]+\\([a-z]+\\)\\.[a-z]+\\([\\d.\\d\\\\]+\\);\\s*]+")
     private val startLogicBlock by literalToken("{")
-    private val operationOperator by literalToken(".")
-    private val digit by regexToken("^[\\d.\\d]+")
-    private val rightPar by literalToken(")")
-    private val leftPar by literalToken("(")
     private val endLogicBlock by literalToken("}")
-    private val endOfStatement by literalToken(";", ignore = true)
 
     /**
      * logic <name>
      */
-    private val scriptHeadParser by -logicKeyword * (scriptName or identifier) use { ScriptHead(text) }
+    private val scriptHeadParser by logicKeyword use { ScriptHead(text.removePrefix("logic").trim()) }
 
-    /**
-     * .plus(1)
-     */
-    private val operationParser by -operationOperator * identifier * -leftPar * (digit or identifier) * -rightPar map { (name, argument) ->
-        ContainerScriptContext.Operation(name.text, argument.text)
-    }
+    private val logicBlockParser by logicBlock use { text }
 
-    /**
-     * ::urge(eat)[.plus(1)]
-     */
-    private val mutationParser by -contextMutationOperator * identifier * -leftPar * identifier * -rightPar * operationParser map { (type, target, operation) ->
-        ContainerScriptContext.ContextMutation(type.text, target.text, operation)
-    }
-
-    private val filterParser by filter use { text.removeSurrounding("[", "]") }
-
-    /**
-     * { actors[::urge(eat)[.plus(1)]]]; }
-     */
-    private val contextCommandParser by identifier * optional(filterParser) * mutationParser
-    private val statementParser by contextCommandParser map { (context, filter, mutation) ->
-        ScriptStatement(
-            context.text,
-            filter ?: "",
-            mutation.type,
-            mutation.target,
-            mutation.operation.name,
-            mutation.operation.argument
-        )
-    }
-
-    private val statementsParser by -startLogicBlock * separatedTerms(statementParser, endOfStatement) * -endLogicBlock
-    override val rootParser by (scriptHeadParser * statementsParser) map { (scriptHead, statements) ->
+    private val statementsParser by -startLogicBlock * logicBlockParser * -endLogicBlock
+    override val rootParser by (scriptHeadParser * statementsParser) map { (scriptHead, logicBlock) ->
+        val scriptStatementGrammar = ScriptStatementGrammar()
+        val statements = scriptStatementGrammar.parseToEnd(logicBlock)
         ContainerScriptContext(scriptHead, statements)
     }
 }
